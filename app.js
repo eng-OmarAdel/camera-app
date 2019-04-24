fcee5292e9f74c10b0db4775ea76ae6e147c133b
Omar M. Adel <eng.omaradel95@yahoo.com>
2019-04-24 20:50:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Set constraints for the video stream
-var constraints = { video: false, audio: false };
+var constraints = { video: true, audio: false };
 var track = null;
 
 // Define constants
